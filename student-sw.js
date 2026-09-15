b8fe2e58e12506941cc1ef19a785d45c6a80bd92
swarup-digitech <swarup229@gmail.com>
2026-09-15 13:07:40 +0530

    Add files via upload

--- student-sw.js
+++ student-sw.js
@@ -1,5 +1,5 @@
-const CACHE='skh-pwa-v2';
-const APP_SHELL=['/index.html','/student-login.html','/school-login.html','/school-dashboard.html','/student-app.webmanifest','/skh-icon.png'];
+const CACHE='skh-pwa-v3-icon';
+const APP_SHELL=['/index.html','/student-login.html','/school-login.html','/school-dashboard.html','/student-app.webmanifest','/skh-icon.png','/skh-icon-192.png','/skh-icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
